6047bcd547c21da456e99ba40fa86015fae8f5d7
Nahuel Pinzone <Nahuelpinzone18@gmail.com>
2023-11-22 20:38:20 -0300

    Proyecto Final

---
+++ README.txt
@@ -0,0 +1,15 @@
+La aplicación "Simulador de Cambio de Divisas" es una herramienta diseñada para calcular conversiones de divisas de manera sencilla y rápida. Permite a los usuarios ingresar un monto en una divisa, seleccionar tanto la divisa de origen como la de destino, y obtener el monto convertido de una divisa a otra.
+
+Esta aplicación se creó utilizando una combinación de tecnologías y conceptos. Entre ellos se incluyen:
+
+HTML y CSS: Se utilizaron para estructurar y estilizar la interfaz de usuario, creando una experiencia amigable y atractiva.
+
+JavaScript: Se empleó para la lógica de la aplicación, manejando las solicitudes a la API de tasas de cambio, procesando la conversión de divisas y actualizando dinámicamente la interfaz de usuario con los resultados.
+
+Fetch/Axios: Se utilizó para realizar solicitudes a una API externa, permitiendo obtener las tasas de cambio en tiempo real entre diferentes divisas.
+
+Eventos y Manipulación del DOM: Los eventos del usuario, como clics y selecciones, se capturaron para manejar las acciones de conversión. Se manipuló el DOM para reflejar los cambios y mostrar los resultados de manera dinámica en la interfaz.
+
+Bootstrap: Se implementó para mejorar la apariencia y la disposición de los elementos, permitiendo un diseño responsivo y una estructura más organizada.
+
+El objetivo principal de la aplicación es proporcionar una solución interactiva y útil para usuarios que requieran calcular conversiones de divisas de forma práctica y eficiente.
---
+++ app.js
@@ -0,0 +1,82 @@
+const API_KEY = '07f73cec1664627fd1ee5bf319b08c12';
+
+document.addEventListener("DOMContentLoaded", () => {
+    obtenerDivisas();
+    document.getElementById('toggleBtn').addEventListener('click', alternarDivisas);
+});
+
+function obtenerDivisas() {
+    axios.get(`https://open.er-api.com/v6/latest?apikey=${API_KEY}`)
+        .then(response => {
+            const data = response.data;
+            const currencies = Object.keys(data.rates);
+            const fromCurrencySelector = document.getElementById("fromCurrency");
+            const toCurrencySelector = document.getElementById("toCurrency");
+            currencies.forEach(currency => {
+                const optionFrom = document.createElement("option");
+                optionFrom.value = currency;
+                optionFrom.text = currency;
+
+                const optionTo = document.createElement("option");
+                optionTo.value = currency;
+                optionTo.text = currency;
+
+                fromCurrencySelector.appendChild(optionFrom);
+                toCurrencySelector.appendChild(optionTo);
+            });
+        })
+        .catch(error => {
+            console.error('Error al cargar las divisas:', error);
+            alert('Hubo un problema al cargar las divisas. Por favor, intenta de nuevo más tarde.');
+        });
+}
+
+function alternarDivisas() {
+    const fromCurrency = document.getElementById('fromCurrency').value;
+    const toCurrency = document.getElementById('toCurrency').value;
+
+    document.getElementById('fromCurrency').value = toCurrency;
+    document.getElementById('toCurrency').value = fromCurrency;
+}
+
+document.getElementById("convertirBtn").addEventListener("click", function () {
+    const fromCurrency = document.getElementById("fromCurrency").value;
+    const toCurrency = document.getElementById("toCurrency").value;
+    const monto = parseFloat(document.getElementById("monto").value);
+
+    if (fromCurrency && toCurrency && !isNaN(monto)) {
+        convertirMoneda(fromCurrency, toCurrency, monto);
+    } else {
+        alert("Por favor, complete todos los campos con valores válidos.");
+    }
+});
+
+function convertirMoneda(from, to, monto) {
+    axios.get(`https://api.exchangeratesapi.io/latest?base=${from}&symbols=${to}&access_key=${API_KEY}`)
+        .then(response => {
+            const data = response.data;
+            if (data.error) {
+                throw new Error(JSON.stringify(data.error));
+            }
+            const tasaCambio = data.rates[to];
+            if (tasaCambio) {
+                const resultado = calcularConversion(monto, tasaCambio);
+                mostrarResultado(resultado, to);
+            } else {
+                throw new Error('No se encontró la tasa de cambio para la conversión.');
+            }
+        })
+        .catch(error => {
+            console.error('Error al realizar la conversión:', error);
+            alert('Hubo un problema al realizar la conversión. Por favor, intenta de nuevo más tarde.');
+        });
+}
+
+function calcularConversion(monto, tasa) {
+    return monto * tasa;
+}
+
+function mostrarResultado(resultado, to) {
+    const resultadoElement = document.getElementById("resultado");
+    resultadoElement.textContent = `El resultado es: ${resultado.toFixed(2)} ${to}`;
+}
